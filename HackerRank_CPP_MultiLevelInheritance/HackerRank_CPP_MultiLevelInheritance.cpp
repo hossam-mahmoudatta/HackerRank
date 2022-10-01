c7245fb7b3582afd566292ed3083f822eca24b82
Hossam Mahmoud <hossam.uddin1@gmail.com>
2022-10-01 10:34:55 +0200

    Pushing the submitted code

--- HackerRank_CPP_MultiLevelInheritance/HackerRank_CPP_MultiLevelInheritance.cpp
+++ HackerRank_CPP_MultiLevelInheritance/HackerRank_CPP_MultiLevelInheritance.cpp
@@ -1,20 +1,37 @@
-// HackerRank_CPP_MultiLevelInheritance.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+class Triangle {
+public:
+    void triangle() {
+        cout << "I am a triangle\n";
+    }
+};
+
+class Isosceles : public Triangle {
+public:
+    void isosceles() {
+        cout << "I am an isosceles triangle\n";
+    }
+};
+
+class Equilateral : public Isosceles {
+public:
+    void equilateral() {
+        cout << "I am an equilateral triangle\n";
+    }
+};
+
+int main() {
+
+    Equilateral eqr;
+    eqr.equilateral();
+    eqr.isosceles();
+    eqr.triangle();
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
