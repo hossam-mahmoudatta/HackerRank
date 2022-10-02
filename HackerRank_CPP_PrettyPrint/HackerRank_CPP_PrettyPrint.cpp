84d6138c534d20cd1719a3b59e068e69bb0a0928
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2022-10-02 22:56:58 +0200

    Pushing the submitted code

--- HackerRank_CPP_PrettyPrint/HackerRank_CPP_PrettyPrint.cpp
+++ HackerRank_CPP_PrettyPrint/HackerRank_CPP_PrettyPrint.cpp
@@ -13,15 +13,13 @@ int main() {
 		double C; cin >> C;
 
 		/* Enter your code here */
-		cout << "A Value\n" << left << setfill('#')
-			<< setw(12) << hex << showbase << 42 << '\n'
-			<< setw(12) << hex << showbase << 42 << '\n';
-		
-		cout << "B Value\n" << right << setfill('_')
-			<< setw(15) << hex << showbase << 42 << '\n';
 
-		cout << "C Value\n" << right << setfill('_')
-			<< setw(15) << hex << showbase << 42 << '\n';
+		cout << left << hex << showbase << nouppercase << (long long)A << '\n';
+
+		cout << right << setfill('_') << setw(15)
+			<< fixed << setprecision(2) << showpos << B << '\n';
+
+		cout << setprecision(9) << scientific << noshowpos << uppercase << C << '\n';
 
 	}
 	return 0;
