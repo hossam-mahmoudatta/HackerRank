508ebcad12d0a12d6fc8f4aed50d3350c14579bc
Hossam Mahmoud <hossam.uddin1@gmail.com>
2022-10-03 14:07:30 +0200

    Updating code

--- HackerRank_CPP_ExceptionalServer/HackerRank_CPP_ExceptionalServer.cpp
+++ HackerRank_CPP_ExceptionalServer/HackerRank_CPP_ExceptionalServer.cpp
@@ -1,20 +1,42 @@
-// HackerRank_CPP_ExceptionalServer.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <exception>
+#include <string>
+#include <stdexcept>
+#include <vector>
+#include <cmath>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class Server {
+private:
+	static int load;
+public:
+	static int compute(long long A, long long B) {
+		load += 1;
+		if (A < 0) {
+			throw std::invalid_argument("A is negative");
+		}
+		vector<int> v(A, 0);
+		int real = -1, cmplx = sqrt(-1);
+		if (B == 0) throw 0;
+		real = (A / B) * real;
+		int ans = v.at(B);
+		return real + A - B * ans;
+	}
+	static int getLoad() {
+		return load;
+	}
+};
+int Server::load = 0;
+
+int main() {
+	int T; cin >> T;
+	while (T--) {
+		long long A, B;
+		cin >> A >> B;
+
+		/* Enter your code here. */
+
+	}
+	cout << Server::getLoad() << endl;
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
