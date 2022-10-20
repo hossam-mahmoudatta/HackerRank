c54a48c5561aae14c695ce50fb12796030f62343
Hossam Mahmoud <hossam.uddin1@gmail.com>
2022-10-20 14:50:55 +0200

    submitted code

--- HackerRank_C_PlayingChars/HackerRank_C_PlayingChars.cpp
+++ HackerRank_C_PlayingChars/HackerRank_C_PlayingChars.cpp
@@ -1,20 +1,28 @@
-// HackerRank_C_PlayingChars.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
-#include <iostream>
+#define MAX_LENGTH 100
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char ch;
+    char s[MAX_LENGTH];
+    char sen[MAX_LENGTH];
+
+    scanf("%c", &ch);
+    getchar(); // to eat the newline command
+
+    fgets(s, MAX_LENGTH, stdin);
+    
+    fgets(sen, MAX_LENGTH, stdin);
+    
+    printf("%c\n", ch);
+    fputs(s, stdout); //fputs not the normal puts so that it wont create a newline
+    fputs(sen, stdout);
+    
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
