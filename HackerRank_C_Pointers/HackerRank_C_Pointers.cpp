e5d5e95ba081722103e43ee9a8ab578627da1ef4
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2022-10-20 19:04:14 +0200

    f

--- HackerRank_C_Pointers/HackerRank_C_Pointers.cpp
+++ HackerRank_C_Pointers/HackerRank_C_Pointers.cpp
@@ -1,20 +1,23 @@
-// HackerRank_C_Pointers.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <stdio.h>
+#include <stdlib.h>
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+void update(int* a, int* b) {
+    // Complete this function    
+    int sum, abso;
+    sum = (*a) + (*b);
+    abso = (*a) - (*b);
+    *a = sum;
+    *b = abs(abso);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
+    int a, b;
+    int *pa = &a;
+    int *pb = &b;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
