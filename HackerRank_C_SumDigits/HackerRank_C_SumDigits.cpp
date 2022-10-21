e43ce572cc1446335b8a16b0a4589048fae7c517
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2022-10-22 01:56:01 +0200

    uploading attempt

---
+++ HackerRank_C_SumDigits/HackerRank_C_SumDigits.cpp
@@ -0,0 +1,24 @@
+
+// Problem Link
+// https://www.hackerrank.com/challenges/sum-of-digits-of-a-five-digit-number/problem?isFullScreen=true
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    //Complete the code to calculate the sum of the five digits on n.
+    int i = 0;
+    int sum = 0;
+    while (i < 5) {
+        n = n % 10;
+        sum += n;
+        
+        i++;
+    }
+    printf("The sum of the digits is: %d", sum);
+    return 0;
+}
