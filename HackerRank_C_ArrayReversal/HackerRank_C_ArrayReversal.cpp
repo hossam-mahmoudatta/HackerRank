bbce04d4bb5a942cc1dad3a1e49cfa5f50d60dbe
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2022-10-21 11:14:05 +0200

    uploading for the first time

--- HackerRank_C_ArrayReversal/HackerRank_C_ArrayReversal.cpp
+++ HackerRank_C_ArrayReversal/HackerRank_C_ArrayReversal.cpp
@@ -1,20 +1,22 @@
-// HackerRank_C_ArrayReversal.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int num, * arr, i;
+    scanf("%d", &num);
+    arr = (int*)malloc(num * sizeof(int));
+    for (i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+    i = 0;
+    while (i < (num / 2)) {
+
+    }
+
+    /* Write the logic to reverse the array. */
+
+    for (i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
