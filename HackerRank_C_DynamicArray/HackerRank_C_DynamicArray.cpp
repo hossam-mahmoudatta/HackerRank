6492db3d0677a09652cbb0df3902aca8aff62cd9
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2022-10-22 21:54:29 +0200

    Submitted this code version

--- HackerRank_C_DynamicArray/HackerRank_C_DynamicArray.cpp
+++ HackerRank_C_DynamicArray/HackerRank_C_DynamicArray.cpp
@@ -1,12 +1,17 @@
+// Problem Link
+// https://www.hackerrank.com/challenges/dynamic-array-in-c/problem?isFullScreen=false
+// I dont understand the final lines of code solutions..
 #include <stdio.h>
 #include <stdlib.h>
 
 // This stores the total number of books in each shelf.
 int* total_number_of_books;
+// So that I can read what the value the address is holding?
 
 // This stores the total number of pages in each book of each shelf.
 //The rows represent the shelves and the columns represent the books.
 int** total_number_of_pages;
+// Why is this a pointer to a pointer to an integer?
 
 int main()
 {
@@ -18,21 +23,43 @@ int main()
     int total_number_of_queries;
     scanf("%d", &total_number_of_queries);
 
+    // I will create dynamic arrays of the books and pages by allocating memory for them.
+    total_number_of_books = (int*)malloc(sizeof(int) * total_number_of_shelves);
+    total_number_of_pages = (int**)malloc(sizeof(int *) * total_number_of_shelves);
+
+    // Now i will initialize the values of the allocated memory by zero.
+    for (int i = 0; i < total_number_of_shelves; i++) {
+        // initialize books by zero
+        total_number_of_books[i] = 0;
+
+        // But why did i allocate here again for the pages?
+        total_number_of_pages[i] = (int*)malloc(sizeof(int)); // ?
+    }
 
     while (total_number_of_queries--) {
         int type_of_query;
-
         // The type of query either its 1, 2, or 3
         scanf("%d", &type_of_query);
 
         if (type_of_query == 1) {
-            /*
-             * Process the query of first type here.
-             */
+            
+            // Process the query of first type here.
             int x, y;
             scanf("%d %d", &x, &y);
+            
+            // int x is the number of the shelve, so if i wanted this query
+            // then that means i need to increment the count of books 
+            // by one each time i do this query.
+            total_number_of_books[x] += 1;
+            
+            // Why did i reallocate here for the pages again?
+            // these two commands i really dont understand them..
+            total_number_of_pages[x] = realloc(total_number_of_pages[x], 
+                total_number_of_books[x] * sizeof(int));
+            total_number_of_pages[x][total_number_of_books[x] - 1] = y;
 
         }
+
         else if (type_of_query == 2) {
             int x, y;
             scanf("%d %d", &x, &y);
