c4c8b22167419e87c3b27ee6fc421fe155d90989
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2022-10-28 23:53:10 +0200

    Submitted Code

--- HackerRank_C_QueryingDocument/HackerRank_C_QueryingDocument.cpp
+++ HackerRank_C_QueryingDocument/HackerRank_C_QueryingDocument.cpp
@@ -1,5 +1,6 @@
 // Problem Link
 // https://www.hackerrank.com/challenges/querying-the-document/problem
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,19 +8,90 @@
 #define MAX_CHARACTERS 1005
 #define MAX_PARAGRAPHS 5
 
-char* kth_word_in_mth_sentence_of_nth_paragraph(char**** document, int k, int m, int n) {
+// This problem gonna be hard
 
+// Creating a function to split a string
+char** split_string(char* text, char delim) {
+    // The idea here is to use strtok, and to use strtok, we need a delimiter
+    // Dynamically allocating memory space for string
+    char** resultString = (char**)malloc(1 * sizeof(char*));
+    int size = 1;
+
+    char* temp = strtok(text, &delim);
+    *resultString = temp;
+
+    while (temp != NULL) {
+        size++;
+        // I'm changing the size of the string as i go
+        resultString = (char**)realloc(resultString, size * sizeof(char*));
+        temp = strtok(NULL, &delim);
+        //Now I'm storing the word inside the result
+        resultString[size - 1] = temp;
+    }
+    return resultString;
+}
+
+char* kth_word_in_mth_sentence_of_nth_paragraph(char**** document, int k, int m, int n) {
+    return document[n - 1][m - 1][k - 1];
 }
 
 char** kth_sentence_in_mth_paragraph(char**** document, int k, int m) {
-
+    return document[m - 1][k - 1];
 }
 
 char*** kth_paragraph(char**** document, int k) {
-
+    return document[k - 1];
 }
 
 char**** get_document(char* text) {
+    // All the logic of the problem is in this function
+    char**** documentHM = NULL;
+    // To identify a paragraph, I have to detect the newline after a sentence end
+    char** paragraphHM = split_string(text, '\n');
+    
+    // Here I will be counting the amount of paragraphs I have
+    int paraNumber = 0;
+    while (paragraphHM[paraNumber] != NULL) {
+        paraNumber++;
+    }
+
+    // Here I allocated space for the document
+    documentHM = (char****)malloc((paraNumber + 1) * sizeof(char***));
+    documentHM[paraNumber] = NULL;
+
+    // Document [Paragraphs][Sentences][Words]
+    // Remember this is my array
+
+    int i = 0;
+    while (paragraphHM[i] != NULL) {
+        char** sentenceHM = split_string(paragraphHM[i], '.');
+        int sentenceNumber = 0;
+        while (sentenceHM[sentenceNumber] != NULL) {
+            sentenceNumber++;
+        }
+
+        documentHM[i] = (char***)malloc((sentenceNumber + 1) * sizeof(char**));
+        documentHM[i][sentenceNumber] = NULL;
+        // As u saw from the previous block of code, its all about
+        // allocating memory correctly for our arrays
+
+        int j = 0;
+        while (sentenceHM[j] != NULL) {
+            documentHM[i][j] = split_string(sentenceHM[j], ' ');
+            j++;
+        }
+
+        i++;
+    }
+    // What should be my next step,
+    // I allocated my memory, now i need to store the input in variables
+    // If we think about it, a char makes a word,
+    // a word makes a sentence, a sentence makes a paragraph
+    // a paragraph makes a document
+    // So if i created a multi-dimensional array
+    // Document [Paragraphs][Sentences][Words][Letters]
+    // So I need to allocate space for a multidimensional array
+    return documentHM;
 
 }
 
@@ -30,6 +102,8 @@ char* get_input_text() {
 
     char p[MAX_PARAGRAPHS][MAX_CHARACTERS], doc[MAX_CHARACTERS];
     memset(doc, 0, sizeof(doc));
+
+    // Input Character
     getchar();
     for (int i = 0; i < paragraph_count; i++) {
         scanf("%[^\n]%*c", p[i]);
@@ -43,12 +117,15 @@ char* get_input_text() {
     return returnDoc;
 }
 
+// Prints the word
 void print_word(char* word) {
     printf("%s", word);
 }
 
+// Prints a list of words, a sentence
 void print_sentence(char** sentence) {
     int word_count;
+    // Gets the number of words to print
     scanf("%d", &word_count);
     for (int i = 0; i < word_count; i++) {
         printf("%s", sentence[i]);
@@ -57,6 +134,7 @@ void print_sentence(char** sentence) {
     }
 }
 
+// Prints sentences from a paragraph
 void print_paragraph(char*** paragraph) {
     int sentence_count;
     scanf("%d", &sentence_count);
