96f3ba80f23291cd6c00eed4421a0566c31e7777
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2022-10-30 22:32:06 +0200

    Uploading submitted code

--- HackerRank_C_PrintingTokens/HackerRank_C_PrintingTokens.cpp
+++ HackerRank_C_PrintingTokens/HackerRank_C_PrintingTokens.cpp
@@ -8,15 +8,25 @@
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    char *s = (char *)malloc(1024 * sizeof(char));
-    scanf("%[^\n]", s);
-    s = (char*)realloc(s, strlen(s) + 1);
+    
+    // Allocating the size for the sentence as the max of 1000 chars
+    char *sentence = (char *)malloc(1024 * sizeof(char));
+    // Getting user input untill newline char
+    scanf("%[^\n]", sentence);
+    // Reallocating memory to the exact length of the sentence
+    sentence = (char*)realloc(sentence, strlen(sentence) + 1);
 
     //Write your logic to print the tokens of the sentence here.
-    for (int i = 0; i < strlen(s); i++) {
-        printf("%s", s[i]);
+    for (int i = 0 ; i < strlen(sentence) ; i++) {
+        if (sentence[i] == ' ') {
+            printf("\n");
+        }
+        else {
+            printf("%c", sentence[i]);
+        }
     }
+    printf("\n");
     
-    free(s);
+    free(sentence);
     return 0;
 }
