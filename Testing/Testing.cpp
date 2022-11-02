d8676d89304642559e2db4f78ba96b112ee25e14
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2022-11-02 03:28:44 +0200

    Testing the Code for SortingArray Application

--- Testing/Testing.cpp
+++ Testing/Testing.cpp
@@ -6,6 +6,50 @@
 #include<assert.h>
 
 int main() {
+	
+	printf("Size of the array of strings: \n");
+	int n;
+	scanf("%d", &n);
+
+	char** arr;
+	arr = (char**)malloc(n * sizeof(char*));
+
+	for (int i = 0; i < n; i++) {
+		*(arr + i) = (char*)malloc(1024 * sizeof(char));
+		scanf("%s", *(arr + i));
+		*(arr + i) = (char*)realloc(*(arr + i), strlen(*(arr + i)) + 1);
+	}
+	
+	char* temp;
+	temp = (char*)malloc(1 * sizeof(char*));
+	temp = *arr;
+	printf("Temp: %s\n", temp);
+	printf("Printing now the array of strings: \n");
+
+	for (int i = 0; i < n; i++) {
+		printf("\n%d: %s\n", (i + 1), *(arr + i)); // reading whats inside the address place
+		printf("Printing now characters of the word: \n");
+		
+		for (int j = 0; j < strlen(*(arr + i)); j++) {
+			// *(&arr[j] + i) actually prints garbage in the place of the characters
+			// Turns out that *((*(arr + i)) + j) is what will print the chars inside the string
+			// And turns out that arr[i][j] works too!
+			// To explain, *(arr + i) means arr[i]
+			// And *((*(arr + i)) + j) means arr[i][j]
+			printf("%d.%d: %c\n", (i + 1), (j + 1), *((*(arr + i)) + j));
+		}
+		printf("Temp: %c %c\n", temp[2], temp[3]);
+		// Now that i know how to access everything using pointers
+		// I now need to compare
+		//if (temp)
+	}
+}
+
+
+
+
+
+/*
 	char** p;
 	p = (char**)malloc(100);
 	p[0] = (char*)"Apple";      // or write *p, points to location of 'A'
@@ -19,7 +63,7 @@ int main() {
 	printf("3. Prints the *p++\n");
 	printf("%s\n", *p);
 
-	/*
+
 		1. Prints the *p
 		Apple
 		2. Prints the **p
@@ -27,4 +71,3 @@ int main() {
 		3. Prints the *p++
 		Banana
 	*/
-}
