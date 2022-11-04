608b75b3cad67ecee5218951c9568264da5c33eb
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2022-11-04 03:52:09 +0200

    Added some comments

--- HackerRank_C_Triangles/HackerRank_C_Triangles.cpp
+++ HackerRank_C_Triangles/HackerRank_C_Triangles.cpp
@@ -14,14 +14,22 @@ typedef struct triangle triangle;
 
 double area(triangle tri) {
 	int a, b, c;
+	// The following are given formulas
+	// to find the areas
+	// The purpose of this function is to
+	// achieve a modular design in coding
 	double p = ((tri.a + tri.b + tri.c) / 2.0);
 	double S = sqrt(p * (p - tri.a) * (p - tri.b) * (p - tri.c));
 	return S;
 }
+
 void sort_by_area(triangle* tr, int n) {
 	// Sort an array a of the length n
+	// Creating a temp variable to hold the data for the swap
 	triangle temp;
 
+	// i < n - 1 so that i dont access forbidden
+	// memory when iterating thru 'j'
 	for (int i = 0; i < n - 1; i++) {
 		for (int j = i + 1 ; j < n; j++) {
 			if (area(tr[i]) > area(tr[j])) {
@@ -31,42 +39,6 @@ void sort_by_area(triangle* tr, int n) {
 			}
 		}
 	}
-	printf("The areas i have are: \n");
-	for (int i = 0; i < n; i++) {
-		printf("%f ", area(tr[i]));
-	}
-	printf("\n");
-	/*
-		if (s[i] > s[i + 1]) {
-			temp->a = tr[i + 1].a;
-			temp->b = tr[i + 1].b;
-			temp->c = tr[i + 1].c;
-
-			tr[i + 1].a = tr[i].a;
-			tr[i + 1].b = tr[i].b;
-			tr[i + 1].c = tr[i].c;
-
-			tr[i].a = temp->a;
-			tr[i].b = temp->b;
-			tr[i].c = temp->c;
-		}
-	*/
-	/*
-		if (s[i] > s[i + 1]) {
-			temp->a = tr[j].a;
-			temp->b = tr[j].b;
-			temp->c = tr[j].c;
-
-			tr[j].a = tr[i].a;
-			tr[j].b = tr[i].b;
-			tr[j].c = tr[i].c;
-
-			tr[i].a = temp->a;
-			tr[i].b = temp->b;
-			tr[i].c = temp->c;
-			break;
-		}
-	*/
 }
 
 int main() {
