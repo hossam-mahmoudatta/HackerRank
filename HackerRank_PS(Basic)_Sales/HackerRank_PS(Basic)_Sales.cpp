d534fe1fa78f8f3ab23b9184f7f693e9a07fa367
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-04-27 22:37:32 +0200

    Update HackerRank_PS(Basic)_Sales.cpp

--- HackerRank_PS(Basic)_Sales/HackerRank_PS(Basic)_Sales.cpp
+++ HackerRank_PS(Basic)_Sales/HackerRank_PS(Basic)_Sales.cpp
@@ -1,20 +1,122 @@
-// HackerRank_PS(Basic)_Sales.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+/*
+int sockMerchant(int n, int ar_count, int* ar) {
+    // I have n elements
+    int pair = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (ar[i] == ar[j]) {
+                ar_count++;
+            }
+        }
+    }
+    pair = ar_count / 2;
+    return pair;
+}
+*/
+// Problem Link
+// https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&isFullScreen=true&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=warmup
+#include <bits/stdc++.h>
 #include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+using namespace std;
+
+string ltrim(const string&);
+string rtrim(const string&);
+vector<string> split(const string&);
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+int sockMerchant(int n, vector<int> ar) {
+    int pair = 0;
+    map<int, int> pairMap;
+    // I created a Map, that will store values and their occurences.
+
+    for (int i = 0; i < n; i++) {
+        pairMap[ar[i]]++;
+    } // Here I inserted the elements from the array or vector into the map
+
+    for (auto counter : pairMap) {
+        pair += counter.second / 2;
+    } // Here I accessed the occurences column of the elements inside the map
+    // so i divided by two to get pairs, then I added into the pairs value.
+    return pair;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string ar_temp_temp;
+    getline(cin, ar_temp_temp);
+
+    vector<string> ar_temp = split(rtrim(ar_temp_temp));
+
+    vector<int> ar(n);
+
+    for (int i = 0; i < n; i++) {
+        int ar_item = stoi(ar_temp[i]);
+
+        ar[i] = ar_item;
+    }
+
+    int result = sockMerchant(n, ar);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+string ltrim(const string& str) {
+    string s(str);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string& str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string& str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
