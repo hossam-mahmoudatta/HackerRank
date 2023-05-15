1c5f9bf3a1713033a2e1985fd02eb9892a84d952
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-05-16 00:59:12 +0300

    Working Code

--- HackerRank_PS(Intermediate)_SortedSums/HackerRank_PS(Intermediate)_SortedSums.cpp
+++ HackerRank_PS(Intermediate)_SortedSums/HackerRank_PS(Intermediate)_SortedSums.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-const int MOD = 1000000007;
+const int MOD = 1000000007; // 1,000,000,007
 
 int sortedSum(const vector<int>& a) {
     
@@ -15,24 +15,26 @@ int sortedSum(const vector<int>& a) {
     
     // a new vector to take a copy of a
     vector<int> sorted(a.begin(), a.end());
-    
-    // Sort the sequence in non-descending order
-    sort(sorted.begin(), sorted.end());  
 
     long long sum = 0;
     // Using long long to avoid overflow during computations
 
     for (int i = 0; i < inputSize; i++) {
         long long fi = 0;  // Current f(i) value
+        
+        // This piece of algorithm will sort as I increase 
+        sort(sorted.begin(), sorted.begin() + i + 1);
 
         for (int j = 0; j <= i; j++) {
-            fi = (fi + (j + 1) * sorted[j]) % MOD;  // Calculate the partial sum for f(i)
+            fi = fi + (j + 1) * sorted[j];
         }
-
-        sum = (sum + fi) % MOD;  // Add the partial sum to the overall sum
+        sum = (sum + fi); // Add the partial sum to the overall sum
     }
-
-    return static_cast<int>(sum);  // Convert the sum to int before returning
+    
+    sum = sum % MOD;
+    
+    // Convert the sum to int before returning
+    return static_cast<int>(sum);  
 }
 
 int main() {
